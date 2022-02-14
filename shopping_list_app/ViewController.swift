5f8e709710de27a5d560376b408f9c213a343d8f
mujicapm <mauro.mujica.3@gmail.com>
2022-02-14 12:12:38 -0600

    Initial Commit

---
+++ shopping_list_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  shopping_list_app
+//
+//  Created by Mauro Mujica-Parodi on 2/14/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
